58801eadb798b0d03e72d090e93fa94a915509b8
Thiago Tavares <thiago.tlopess@gmail.com>
2024-10-21 18:46:18 -0300

    att

---
+++ gamlss.R
@@ -0,0 +1,11 @@
+# Estudo sobre o gammls - usando o conteudo https://rpubs.com/Citro/GAMLSS
+
+library(gamlss)
+library(tidyverse)
+library(ggpubr)
+#library(skimr)
+
+
+data('rent')
+dados<-rent
+View(dados)
