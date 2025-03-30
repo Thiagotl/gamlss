70ab3e51a8486bee571c7808607fe9a846bb98b5
Thiago Tavares <thiago.tlopess@gmail.com>
2025-03-29 21:03:40 -0300

    att

--- gamlss.R
+++ gamlss.R
@@ -9,3 +9,49 @@ library(ggpubr)
 data('rent')
 dados<-rent
 View(dados)
+
+
+
+model <- gamlss(R ~ Fl+A+H+loc, family=NO, data=rent, trace=FALSE)
+
+plot(rent)
+
+rent<-as.data.frame(rent)
+
+fitted(model, "sigma")
+
+model$sigma.link
+model$sigma.coefficients
+
+residuals(model)
+
+plot(model)
+
+
+
+set.seed(123)
+
+n <- 100
+x <- runif(n, 1, 10)
+y <- rgamma(n, shape = 2, rate = 0.5 + 0.1 * x)  # média = shape / rate
+
+df <- data.frame(x, y)
+modelo_glm <- glm(y ~ x, family = Gamma(link = "inverse"), data = df)
+summary(modelo_glm)
+
+
+
+modelo_gamlss <- gamlss(y ~ x, family = GA, data = df)  # GA = Gama
+summary(modelo_gamlss)
+
+modelo_gamlss$mu.link
+
+
+
+
+
+
+
+
+
+
